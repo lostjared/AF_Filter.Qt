b53a91f6823952523ea7b7387f8c62fc670f5499
lostjared <jaredbruni@gmail.com>
2017-09-01 06:21:16 -0700

    added prototypes

--- src/alphaflame.h
+++ src/alphaflame.h
@@ -17,4 +17,28 @@ void changePixel(unsigned int current_filterx, QImage &full_buffer, int i, int z
 
 void alphaFlame(QImage &image, bool neg_state, unsigned int red, unsigned int green, unsigned int blue, int reverse, int filter_num, int iteration);
 
+
+unsigned char *pixelAt(const QImage &image, int x, int y, unsigned int &value);
+void ApplyOptions(unsigned char *buffer, bool negated, int red, int green, int blue, int reverse);
+void GlitchSort(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
+void GlitchSortByChannel(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
+void GlitchSortVertical(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
+void UniqueMirrorBlend(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
+void Reverse(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
+void BlendScanLines(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
+void UpDown(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
+void LeftRight(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void XorMultiBlend(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void GradientLines(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void GradientSelf(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void Side2Side(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void Top2Bottom(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void Outward(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void OutwardSquare(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void ShiftPixels(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void ShiftPixelsDown(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+void BlendWithSource(QImage &image, bool neg, unsigned int iteration, unsigned int red, unsigned int green, unsigned int blue, int rev);
+
+
 #endif
+
