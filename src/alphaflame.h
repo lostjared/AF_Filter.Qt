1446b585c487ad03377e4e608f0f7bd11280565d
lostjared <jaredbruni@gmail.com>
2017-09-01 09:59:00 -0700

    changed from QtCore to QImage

--- src/alphaflame.h
+++ src/alphaflame.h
@@ -9,15 +9,12 @@
 #ifndef __ALPHA_FLAME_H___
 #define __ALPHA_FLAME_H___
 
-#include<QtCore>
+#include<QImage>
 
 extern QImage start_image;
 
 void changePixel(unsigned int current_filterx, QImage &full_buffer, int i, int z, unsigned char *buffer, double pos, double *count, int reverse, bool negated, unsigned int red, unsigned int green, unsigned int blue, unsigned int randomNumber);
-
 void alphaFlame(QImage &image, bool neg_state, unsigned int red, unsigned int green, unsigned int blue, int reverse, int filter_num, int iteration);
-
-
 unsigned char *pixelAt(const QImage &image, int x, int y, unsigned int &value);
 void ApplyOptions(unsigned char *buffer, bool negated, int red, int green, int blue, int reverse);
 void GlitchSort(QImage &image, bool neg, int iteration, int red, int green, int blue, int rev);
